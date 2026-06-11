19052d1008c3f26a36f375f9e4bb5fa5a9984f7d
Adarsh Pandey <adarsh.pandey@finhaat.in>
2026-06-11 16:53:50 +0530

    sd

--- src/App.js
+++ src/App.js
@@ -3,12 +3,9 @@ import ProtectedRoute from "./routes/ProtectedRoute";
 import PublicRoute from "./routes/PublicRoute";
 import UserLogin from "./screens/UserLogin/UserLogin";
 import UserSignUp from "./screens/UserLogin/UserSignUp";
-import DigitalGold from "./screens/DigitalGold/DigitalGoldRoutes";
-import Dashboard from "./screens/Dashboard/Dashboard";
 import { Toaster } from "react-hot-toast";
-import Profile from "./screens/UserProfile/Profile";
 import ProtectedLayout from "./routes/ProtectedLayout";
-import MainDashboard from "./screens/MainDashboard/MainDashboard";
+import Dashboard from "./screens/MainDashboard/MainDashboard";
 import QRPatientAppointment from './screens/PatientScreen/Appointment/Appointment'
 import AppointmentSuccess from './screens/PatientScreen/Appointment/AppointmentSuccess'
 import ClinicPatientsList from "./screens/ClinicPatients/ClinicPatientsList/ClinicPatientsList";
@@ -43,9 +40,8 @@ const App = () => {
         <Route element={<ProtectedRoute />}>
           <Route path="/complete-profile" element={<CompleteProfile />} />
           <Route element={<ProtectedLayout />}>
-            <Route path="/dashboard" element={<MainDashboard />} />
+            <Route path="/dashboard" element={<Dashboard />} />
             <Route path="/patient-list" element={<ClinicPatientsList />} />
-            <Route path="/digitalGold/*" element={<DigitalGold />} />
             <Route path="/Profile" element={<ClinicProfile />} />
           </Route>
         </Route>
--- src/hooks/useSubscription.js
+++ src/hooks/useSubscription.js
@@ -9,22 +9,22 @@ const useSubscription = () => {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    const today = new Date().toISOString().slice(0, 10);
-    const cachedDate = localStorage.getItem(CACHE_DATE_KEY);
-    const cachedData = localStorage.getItem(CACHE_DATA_KEY);
+    // const today = new Date().toISOString().slice(0, 10);
+    // const cachedDate = localStorage.getItem(CACHE_DATE_KEY);
+    // const cachedData = localStorage.getItem(CACHE_DATA_KEY);
 
-    if (cachedDate === today && cachedData) {
-      setSubscription(JSON.parse(cachedData));
-      setLoading(false);
-      return;
-    }
+    // if (cachedDate === today && cachedData) {
+    //   setSubscription(JSON.parse(cachedData));
+    //   setLoading(false);
+    //   return;
+    // }
 
     (async () => {
       try {
         const res = await fetchSubscription();
         if (res?.success) {
-          localStorage.setItem(CACHE_DATE_KEY, today);
-          localStorage.setItem(CACHE_DATA_KEY, JSON.stringify(res.data));
+          // localStorage.setItem(CACHE_DATE_KEY, today);
+          // localStorage.setItem(CACHE_DATA_KEY, JSON.stringify(res.data));
           setSubscription(res.data);
         }
       } catch {
